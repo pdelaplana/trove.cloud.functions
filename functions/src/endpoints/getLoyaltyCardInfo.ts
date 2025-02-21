52853a8bb4821bc0003e8cb8154259101fe6d5ac
pdelaplana <patrick.delaplana@gmail.com>
2025-02-21 16:19:09 +1000

    test to trigger the pipeline

--- functions/src/endpoints/getLoyaltyCardInfo.ts
+++ functions/src/endpoints/getLoyaltyCardInfo.ts
@@ -12,57 +12,60 @@ import { logger } from 'firebase-functions/v2';
 import { runWithAuthentication } from '@src/shared/helpers/runWithAuthentication';
 import { LoyaltyCard } from '@src/domain';
 
-export const getLoyaltyCardInfo = onRequest(async (req, res) => {
-  const identifier = req.query.membershipId as string | undefined;
-  if (!identifier) {
-    res.status(400).send('Membership number is required');
-    return;
-  }
+export const getLoyaltyCardInfo = onRequest(
+  { memory: '256MiB', timeoutSeconds: 60 },
+  async (req, res) => {
+    const identifier = req.query.membershipId as string | undefined;
+    if (!identifier) {
+      res.status(400).send('Membership number is required');
+      return;
+    }
 
-  beginTimedOperation('getLoyaltyCardInfo', { identifier }, async () => {
-    runWithAuthentication(req, res, async (context) => {
-      const { businessId } = context;
-      let loyaltyCard: LoyaltyCard | null = null;
-      // check if membershipId is a membership number
-      if (identifier?.length === 11) {
-        loyaltyCard = await fetchLoyaltyCardByMembershipNumber(identifier!);
-      }
-      if (!loyaltyCard) {
-        const customer = await fetchCustomerByEmailOrPhone(identifier!);
-        if (!customer) {
-          res.status(404).send('Customer not found');
+    beginTimedOperation('getLoyaltyCardInfo', { identifier }, async () => {
+      runWithAuthentication(req, res, async (context) => {
+        const { businessId } = context;
+        let loyaltyCard: LoyaltyCard | null = null;
+        // check if membershipId is a membership number
+        if (identifier?.length === 11) {
+          loyaltyCard = await fetchLoyaltyCardByMembershipNumber(identifier!);
+        }
+        if (!loyaltyCard) {
+          const customer = await fetchCustomerByEmailOrPhone(identifier!);
+          if (!customer) {
+            res.status(404).send('Customer not found');
+            return;
+          }
+          loyaltyCard = await fetchLoyaltyCardByCustomerAndBusinessId(
+            customer.id,
+            businessId!
+          );
+        }
+        if (!loyaltyCard) {
+          res.status(404).send('Loyalty card not found');
           return;
         }
-        loyaltyCard = await fetchLoyaltyCardByCustomerAndBusinessId(
-          customer.id,
-          businessId!
+        logger.info('Loyalty card found', { loyaltyCard });
+
+        // add information about the customer, business and loyalty information to the response
+        const customer = await fetchCustomerById(loyaltyCard.customerId);
+        const business = await fetchBusinessById(businessId!);
+        const loyaltyProgram = await fetchLoyaltyProgramById(
+          loyaltyCard.loyaltyProgramId,
+          businessId
         );
-      }
-      if (!loyaltyCard) {
-        res.status(404).send('Loyalty card not found');
-        return;
-      }
-      logger.info('Loyalty card found', { loyaltyCard });
 
-      // add information about the customer, business and loyalty information to the response
-      const customer = await fetchCustomerById(loyaltyCard.customerId);
-      const business = await fetchBusinessById(businessId!);
-      const loyaltyProgram = await fetchLoyaltyProgramById(
-        loyaltyCard.loyaltyProgramId,
-        businessId
-      );
-
-      res.status(200).send({
-        ...loyaltyCard,
-        customerName: `${customer.firstName} ${customer.lastName}`,
-        customerEmail: customer.email,
-        customerPhone: customer.phone,
-        businessName: business.name,
-        loyaltyProgramName: loyaltyProgram.name,
-        tierName: loyaltyProgram.tiers.find(
-          (tier) => tier.id === loyaltyCard?.tierId
-        )?.name,
+        res.status(200).send({
+          ...loyaltyCard,
+          customerName: `${customer.firstName} ${customer.lastName}`,
+          customerEmail: customer.email,
+          customerPhone: customer.phone,
+          businessName: business.name,
+          loyaltyProgramName: loyaltyProgram.name,
+          tierName: loyaltyProgram.tiers.find(
+            (tier) => tier.id === loyaltyCard?.tierId
+          )?.name,
+        });
       });
     });
-  });
-});
+  }
+);
