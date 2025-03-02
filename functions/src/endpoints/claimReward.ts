ffd6f5d8d28e79525bb2ec8c00d7960a8b8ca96b
pdelaplana <patrick.delaplana@gmail.com>
2025-03-03 06:35:55 +1000

    refactor claimReward endpoint to use handleRequest middleware

--- functions/src/endpoints/claimReward.ts
+++ functions/src/endpoints/claimReward.ts
@@ -3,9 +3,6 @@ import {
   createFirebaseEventBus,
   setupRewardEventHandlers,
 } from '@src/eventBus';
-import { allowedMethods } from '@src/shared/helpers/allowedMethods';
-import { beginTimedOperation } from '@src/shared/helpers/beginTimedOperation';
-import { runWithAuthentication } from '@src/shared/helpers/runWithAuthentication';
 import {
   fetchBusinessById,
   fetchCustomerById,
@@ -17,85 +14,84 @@ import { fetchCustomerRewardById } from '@src/shared/queries/fetchCustomerReward
 import { fetchCustomerRewards } from '@src/shared/queries/fetchCustomerRewards';
 import { logger } from 'firebase-functions/v2';
 import { onRequest } from 'firebase-functions/v2/https';
-import cors from 'cors';
+import { handleRequest } from '@src/shared/helpers/handleRequest';
 
-export const claimReward = onRequest(async (request, response) => {
-  cors()(request, response, () => {
-    allowedMethods(request, response, ['POST']);
-
-    const { membershipNumber, loyaltyProgramMilestoneId } = request.body;
-
-    const eventBus = createFirebaseEventBus();
-    setupRewardEventHandlers(eventBus);
-
-    beginTimedOperation(
+export const claimReward = onRequest(
+  { memory: '256MiB', timeoutSeconds: 60 },
+  async (request, response) => {
+    handleRequest(
       'claimReward',
-      { membershipNumber, loyaltyProgramMilestoneId },
-      async () => {
-        runWithAuthentication(request, response, async (context) => {
-          const { businessId } = context;
-          const loyaltyCard =
-            await fetchLoyaltyCardByMembershipNumber(membershipNumber);
-          if (!loyaltyCard) {
-            response.status(404).send({ error: 'Loyalty card not found.' });
-            return;
-          }
-          logger.debug('Loyalty card found', { loyaltyCard });
-          if (loyaltyCard.businessId !== businessId) {
-            response.status(403).send({
-              error: 'Loyalty card does not belong to this business.',
-            });
-            return;
-          }
+      request,
+      response,
+      ['POST'],
+      {},
+      async (context) => {
+        const { membershipNumber, loyaltyProgramMilestoneId } = request.body;
 
-          const [business, customer, loyaltyProgramMilestone, customerRewards] =
-            await Promise.all([
-              fetchBusinessById(businessId),
-              fetchCustomerById(loyaltyCard.customerId),
-              fetchLoyaltyProgramMilestoneById(
-                loyaltyProgramMilestoneId,
-                loyaltyCard.loyaltyProgramId,
-                businessId
-              ),
-              fetchCustomerRewards(businessId, loyaltyCard.id),
-            ]);
+        const eventBus = createFirebaseEventBus();
+        setupRewardEventHandlers(eventBus);
 
-          if (!loyaltyProgramMilestone) {
-            response.status(404).send({ error: 'Milestone not found.' });
-            return;
-          }
-
-          const { success, error, data } = await processClaimReward(
-            {
-              loyaltyCard,
-              customer,
-              business,
-              loyaltyProgramMilestone,
-              customerRewards,
-              transactionType: 'redeem',
-            },
-            eventBus
-          );
-
-          if (!success || !data) {
-            response.status(500).send({ error: error });
-            return;
-          }
-
-          response.status(200).send({
-            message: 'Reward claimed',
-            reward: await fetchCustomerRewardById(
-              data.rewardId,
-              businessId,
-              loyaltyCard.id
-            ),
-            transaction: await fetchLoyaltyCardTransactionById(
-              data.transactionId,
+        const { businessId } = context;
+        const loyaltyCard =
+          await fetchLoyaltyCardByMembershipNumber(membershipNumber);
+        if (!loyaltyCard) {
+          response.status(404).send({ error: 'Loyalty card not found.' });
+          return;
+        }
+        logger.debug('Loyalty card found', { loyaltyCard });
+        if (loyaltyCard.businessId !== businessId) {
+          response.status(403).send({
+            error: 'Loyalty card does not belong to this business.',
+          });
+          return;
+        }
+        const [business, customer, loyaltyProgramMilestone, customerRewards] =
+          await Promise.all([
+            fetchBusinessById(businessId),
+            fetchCustomerById(loyaltyCard.customerId),
+            fetchLoyaltyProgramMilestoneById(
+              loyaltyProgramMilestoneId,
+              loyaltyCard.loyaltyProgramId,
               businessId
             ),
-          });
+            fetchCustomerRewards(businessId, loyaltyCard.id),
+          ]);
+
+        if (!loyaltyProgramMilestone) {
+          response.status(404).send({ error: 'Milestone not found.' });
+          return;
+        }
+
+        const { success, error, data } = await processClaimReward(
+          {
+            loyaltyCard,
+            customer,
+            business,
+            loyaltyProgramMilestone,
+            customerRewards,
+            transactionType: 'redeem',
+          },
+          eventBus
+        );
+
+        if (!success || !data) {
+          response.status(500).send({ error: error });
+          return;
+        }
+
+        response.status(200).send({
+          message: 'Reward claimed',
+          reward: await fetchCustomerRewardById(
+            data.rewardId,
+            businessId,
+            loyaltyCard.id
+          ),
+          transaction: await fetchLoyaltyCardTransactionById(
+            data.transactionId,
+            businessId
+          ),
         });
       }
     );
-  });
-});
+  }
+);
